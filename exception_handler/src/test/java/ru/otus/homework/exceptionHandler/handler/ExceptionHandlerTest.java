3c08c8a09afb7dbf86caaacbaefc0468244c8d26
sn.kozlov <sergey.kozlov@tccenter.ru>
2025-10-24 19:32:48 +0300

    added tests

--- exception_handler/src/test/java/ru/otus/homework/exceptionHandler/handler/ExceptionHandlerTest.java
+++ exception_handler/src/test/java/ru/otus/homework/exceptionHandler/handler/ExceptionHandlerTest.java
@@ -1,6 +1,5 @@
 package ru.otus.homework.exceptionHandler.handler;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -10,8 +9,10 @@ import ru.otus.homework.exceptionHandler.command.RetryThrowingCommand;
 import ru.otus.homework.exceptionHandler.command.RetryTwiceThrowingCommand;
 
 import java.io.FileNotFoundException;
+import java.util.ConcurrentModificationException;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,19 +20,8 @@ class ExceptionHandlerTest {
 
     private final static ConcurrentLinkedDeque<Command> QUEUE = new ConcurrentLinkedDeque<>();
 
-    @BeforeEach
-    void setUp() throws Throwable {
-        Command illegalArgumentThrowingCommandMock = mock(Command.class);
-        doThrow(IllegalArgumentException.class).when(illegalArgumentThrowingCommandMock).execute();
-
-        ExceptionHandler.register(
-                illegalArgumentThrowingCommandMock.getClass(),
-                IllegalArgumentException.class,
-                (cmd, ex) -> new RetryThrowingCommand(cmd)
-        );
-
-        QUEUE.push(illegalArgumentThrowingCommandMock);
-
+    @Test
+    public void checkLoggingThrowingCommand() throws Throwable {
         Command runtimeThrowingCommandMock = mock(Command.class);
         doThrow(RuntimeException.class).when(runtimeThrowingCommandMock).execute();
 
@@ -43,30 +33,110 @@ class ExceptionHandlerTest {
 
         QUEUE.push(runtimeThrowingCommandMock);
 
+        assertDoesNotThrow(this::readQueue);
+    }
+
+    @Test
+    public void checkPushLoggingCommandHandler() throws Throwable {
         Command fileNotFoundThrowingCommandMock = mock(Command.class);
         doThrow(FileNotFoundException.class).when(fileNotFoundThrowingCommandMock).execute();
 
         ExceptionHandler.register(
                 fileNotFoundThrowingCommandMock.getClass(),
                 FileNotFoundException.class,
-                (cmd, ex) -> new RetryTwiceThrowingCommand(cmd)
+                (cmd, ex) -> new PushLoggingCommandHandler(ex, QUEUE)
         );
 
-        QUEUE.push(fileNotFoundThrowingCommandMock);
+        try {
+            fileNotFoundThrowingCommandMock.execute();
+        } catch (Throwable e) {
+            ExceptionHandler.handle(fileNotFoundThrowingCommandMock, e).execute();
+        }
+
+        assertAll(
+                () -> assertEquals(1, QUEUE.size()),
+                () -> assertEquals(QUEUE.getFirst().getClass(), LoggingThrowingCommand.class)
+        );
     }
 
     @Test
-    public void handleTest() {
+    public void checkRetryThrowingCommand() throws Throwable {
+        Command illegalArgumentThrowingCommandMock = mock(Command.class);
+        doThrow(IllegalArgumentException.class).when(illegalArgumentThrowingCommandMock).execute();
+
+        ExceptionHandler.register(
+                illegalArgumentThrowingCommandMock.getClass(),
+                IllegalArgumentException.class,
+                (cmd, ex) -> new RetryThrowingCommand(cmd)
+        );
+
+        QUEUE.push(illegalArgumentThrowingCommandMock);
+
+        assertThrows(IllegalArgumentException.class, this::readQueue);
+    }
+
+    @Test
+    public void chetPushRetryThrowingHandler() throws Throwable {
+        Command fileNotFoundThrowingCommandMock = mock(Command.class);
+        doThrow(FileNotFoundException.class).when(fileNotFoundThrowingCommandMock).execute();
+
+        ExceptionHandler.register(
+                fileNotFoundThrowingCommandMock.getClass(),
+                FileNotFoundException.class,
+                (cmd, ex) -> new PushRetryCommandHandler(cmd, QUEUE)
+        );
+
+        try {
+            fileNotFoundThrowingCommandMock.execute();
+        } catch (Throwable e) {
+            ExceptionHandler.handle(fileNotFoundThrowingCommandMock, e).execute();
+        }
+
+        assertAll(
+                () -> assertEquals(1, QUEUE.size()),
+                () -> assertEquals(QUEUE.getFirst().getClass(), RetryThrowingCommand.class)
+        );
+    }
+
+    @Test
+    public void checkLoggingRetryingCommandHandler() throws Throwable {
+        Command runtimeThrowingCommandMock = mock(Command.class);
+        doThrow(RuntimeException.class).when(runtimeThrowingCommandMock).execute();
+
+        ExceptionHandler.register(
+                runtimeThrowingCommandMock.getClass(),
+                RuntimeException.class,
+                (cmd, ex) -> new LoggingRetryingCommandHandler(ex, cmd)
+        );
+
+        QUEUE.push(runtimeThrowingCommandMock);
+
+        assertDoesNotThrow(this::readQueue);
+    }
+
+    @Test
+    public void checkRetryTwiceThrowingCommand() throws Throwable {
+        Command concurrentModificationThrowingCommandMock = mock(Command.class);
+        doThrow(ConcurrentModificationException.class).when(concurrentModificationThrowingCommandMock).execute();
+
+        ExceptionHandler.register(
+                concurrentModificationThrowingCommandMock.getClass(),
+                ConcurrentModificationException.class,
+                (cmd, ex) -> new RetryTwiceThrowingCommand(cmd)
+        );
+
+        QUEUE.push(concurrentModificationThrowingCommandMock);
+
+        assertThrows(ConcurrentModificationException.class, this::readQueue);
+    }
+
+    public void readQueue() throws Throwable {
         while (!QUEUE.isEmpty()) {
             Command command = QUEUE.poll();
             try {
                 command.execute();
             } catch (Throwable e) {
-                try {
-                    ExceptionHandler.handle(command, e).execute();
-                } catch (Throwable t) {
-                    System.out.println("Очередная ошибка");
-                }
+                ExceptionHandler.handle(command, e).execute();
             }
         }
     }
